b0fc762f657671478d0002a6240eb165bc825f2e
HodyahAdler <62502734+HodyahAdler@users.noreply.github.com>
2023-03-29 12:53:56 +0300

    Add files via upload

---
+++ FailingCheckstyleCode.java
@@ -0,0 +1,19 @@
+/**
+ * This is a javadoc.
+ */
+public class FailingCheckstyleCode
+{
+
+   /**
+    *
+    * @param args
+    */
+   public static void main(String[] args) 
+   {
+      for(int I=0;I<10;++I) 
+      {
+         System.out.println("I is:"+I);
+      }
+   }
+
+}
